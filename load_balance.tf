1c0e7c7abbe855bc2781d91f4887339b3356c5cc
AdaSpinal <60679768+AdaSpinal@users.noreply.github.com>
2022-06-23 20:18:14 -0600

    Add files via upload

---
+++ load_balance.tf
@@ -0,0 +1,76 @@
+resource "aws_vpc" "vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_internet_gateway" "gateway" {
+  vpc_id = "${aws_vpc.vpc.id}"
+
+}
+
+data "aws_vpc" "default" {
+    default = true
+}
+
+data "aws_subnet_ids" "subnet" {
+  vpc_id = data.aws_vpc.default.id
+}
+
+resource "aws_alb_target_group" "target-group" {
+    health_check {
+      interval = 10
+      path = "/"
+      protocol = "HTTP"
+      timeout = 5
+      healthy_threshold = 5
+      unhealthy_threshold = 2
+    } 
+    
+      
+
+    name ="ExamenAdaEspTargetGroup"
+    port = 5000
+    protocol = "HTTP"
+    target_type = "instance"
+    vpc_id = data.aws_vpc.default.id
+    stickiness {
+      type = "lb_cookie"
+    }
+    
+  
+}
+
+#Instance Attachment
+resource "aws_autoscaling_attachment" "svc_asg_external2" {
+  alb_target_group_arn = "${aws_alb_target_group.target-group.arn }"
+  autoscaling_group_name = "${aws_autoscaling_group.bar.id}" 
+  
+}
+
+resource "aws_alb" "application-alb" {
+    name = "LB-ExamenAdaEsp"
+    internal  = false
+    ip_address_type = "ipv4"
+    load_balancer_type = "application"
+    security_groups = ["sg-028e0d5f7222fc799"]
+    subnets = data.aws_subnet_ids.subnet.ids
+
+        tags = {
+          Name = "Examen Super Mario"
+        }
+
+}
+
+
+resource "aws_alb_listener" "alb-listener" {
+    load_balancer_arn = aws_alb.application-alb.arn 
+    port              = 80
+    protocol          = "HTTP"
+    default_action{
+        target_group_arn = aws_alb_target_group.target-group.arn 
+        type = "forward"
+    } 
+
+  
+}
+
+
---
+++ userdata.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" 
+nvm install 16
+sudo yum install -y git
+git clone https://github.com/meth-meth-method/super-mario.git
+cd super-mario
+npm install
+npm install pm2 -g
+pm2 start  
+
